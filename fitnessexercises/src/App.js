f2a1e5c5d9365dab0b7b341d90f0aed4bd7c8877
hafsa <99544208+HafsaOuaj@users.noreply.github.com>
2023-06-15 12:28:05 +0200

    the project

--- fitnessexercises/src/App.js
+++ fitnessexercises/src/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import {Route,Routes} from 'react-router-dom';
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import ExerciseDetail from './components/ExerciseDetail'
-import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-
-
-const App=()=> {
-  return (
-    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
-
-
-        </Routes>
-    <Footer/>
-    </Box>
-  )
-}
-
-export default App
+export default App;
